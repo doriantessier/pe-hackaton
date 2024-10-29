7174a3954996b716406e3d27a094a9121c8794cc
Dorian Tessier <doriantessier77@gmail.com>
2024-10-29 16:28:34 +0100

    import xcover

--- dodo.py
+++ dodo.py
@@ -1,4 +1,5 @@
 import numpy as np
+from xcover import covers_bool
 
 # Dictionnaire de formes de pentominos (matrices)
 RAW_SHAPES = {
@@ -93,3 +94,5 @@ output = generate_all_pentomino_positions(pentomino_list)
 # Afficher la forme du résultat
 print(output.shape)  # Affichera (nombre_total_de_positions, 72)
 print(output[:5])  # Afficher les 5 premières configurations pour vérification
+
+
